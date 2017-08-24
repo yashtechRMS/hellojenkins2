402a4bbb475699cdfeb2bb984e83540e89df9602
saajansoni16 <saajansoni@gmail.com>
2017-08-24 14:17:37 +0530

    junit test added

--- newTestJenkinsJob/src/helloworldjenkins/helloworld1.java
+++ newTestJenkinsJob/src/helloworldjenkins/helloworld1.java
@@ -2,10 +2,10 @@ package helloworldjenkins;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 
 public class helloworld1 {
