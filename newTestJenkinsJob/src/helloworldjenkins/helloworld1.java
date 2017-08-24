1ad0084c90ac2a32a25b0f460a8c5944e1ec7700
saajansoni16 <saajansoni@gmail.com>
2017-08-24 14:47:39 +0530

    junit added

--- newTestJenkinsJob/src/helloworldjenkins/helloworld1.java
+++ newTestJenkinsJob/src/helloworldjenkins/helloworld1.java
@@ -7,31 +7,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class helloworld1 {
 
 	static WebDriver driver;
+
 	@Test
-	public static void Hello() {
+	public void Hello() {
 		// set path to chromedriver.exe
-				System.setProperty("webdriver.chrome.driver", "E:\\RMS\\RMS_Dec-22\\src\\test\\resources\\drivers\\chromedriver.exe");
-				// create chrome instance
-				driver = new ChromeDriver();
-				driver.manage().window().maximize();
-				driver.get("http://inidrrmstsrv01:8080/rms");
-				driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-				String title = driver.getTitle();
-				if(title.equalsIgnoreCase("RMS | Dashboard")){
-					System.out.println("User is loged in into RMS app");
-					String name = driver.findElement(By.xpath("//a[@class='dropdown-toggle']/span")).getText();
-					if(name.equalsIgnoreCase("saajan soni")){
-						System.out.println("Logged in user is : "+ name);
-					}else{
-						System.out.println("Invalid user");
-					}
-				}else{
-					System.out.println("user is not logged into rms");
-				}
+		System.setProperty("webdriver.chrome.driver", "E:\\RMS\\RMS_Dec-22\\src\\test\\resources\\drivers\\chromedriver.exe");
+		// create chrome instance
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://inidrrmstsrv01:8080/rms");
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		String title = driver.getTitle();
+		if (title.equalsIgnoreCase("RMS | Dashboard")) {
+			System.out.println("User is loged in into RMS app");
+			String name = driver.findElement(By.xpath("//a[@class='dropdown-toggle']/span")).getText();
+			if (name.equalsIgnoreCase("saajan soni")) {
+				System.out.println("Logged in user is : " + name);
+			} else {
+				System.out.println("Invalid user");
+			}
+		} else {
+			System.out.println("user is not logged into rms");
+		}
 	}
 
 }
---
+++ newTestJenkinsJob/src/helloworldjenkins/helloworld1Test.java
@@ -0,0 +1,14 @@
+package helloworldjenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class helloworld1Test {
+
+	@Test
+	public void testHello() {
+		fail("Not yet implemented");
+	}
+
+}
